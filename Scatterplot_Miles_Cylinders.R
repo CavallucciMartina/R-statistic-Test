769b5771d0f8e9d9e2d7bba229973c27a61f6600
unknown <martina.cavallucci96@gmail.com>
2018-10-05 12:16:33 +0200

    Scatterplot Miles - Cylinders Cars

---
+++ Scatterplot_Miles_Cylinders.R
@@ -0,0 +1,23 @@
+#Install ISLR package and use it
+library(ISLR)
+#Import the Cars Data Frame
+data(Auto)
+#Show the Data Frame
+head(Auto)
+#Show all the headers of the Data Frame
+
+#Show info about Data Frame
+str(Auto)
+
+#Instruct R to look up data in the Auto file with the attach () function
+attach(Auto)
+
+#plot() produce a scatterplot of quantitative variables
+#mpg = miles per gallon
+#cylinders = Number of cylinders between 4 and 8
+
+
+#Traslate quantitative variables in qualitatives variable
+cylinders = as.factor (cylinders)
+
+plot(cylinders,mpg)
