16e3e4ec8cffc8adc0b5266625c619fab2c7caa6
unknown <martina.cavallucci@studio.unibo.it>
2018-12-24 00:32:24 +0100

    refresh read cvs command

--- CreatingSQLiteDBinR.R
+++ CreatingSQLiteDBinR.R
@@ -1,9 +1,9 @@
 #https://datacarpentry.org/R-ecology-lesson/05-r-and-databases.html#creating_a_new_sqlite_database
 library(dplyr)
 library(dbplyr)
-species <- read_csv("data/species.csv")
-surveys <- read_csv("data/surveys.csv")
-plots <- read_csv("data/plots.csv")
+species <- read.csv("data/species.csv")
+surveys <- read.csv("data/surveys.csv")
+plots <- read.csv("data/plots.csv")
 my_db_file <- "portal-database.sqlite"
 my_db <- src_sqlite(my_db_file, create = TRUE)
 my_db
