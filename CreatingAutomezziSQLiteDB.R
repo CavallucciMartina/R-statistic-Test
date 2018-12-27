20c38713d8752b9e8cf3a491e8866e0bcd6ffb62
unknown <martina.cavallucci@studio.unibo.it>
2018-12-27 12:05:17 +0100

    Create and first query

---
+++ CreatingAutomezziSQLiteDB.R
@@ -0,0 +1,4 @@
+library(dplyr)
+library(dbplyr)
+Automezzi <- read.csv("Rdb/Automezzi.csv",header = TRUE, sep = ";")
+AutomezziCarburanti<- read.csv("Rdb/AutomezziCarburanti.csv",header = TRUE, sep = ";")
